a0a9c244215665dbd6d587fc21c61424777a66ee
prem karthik <karthik19596@gmail.com>
2021-04-02 11:52:51 +0530

    Final program

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,9 @@
 package com.mycompany.app;
 
-//**
- * Hello world
- *
- *//
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+public class App {
+    public static void main( String[] args ){
+    
         System.out.println( "Hello World!" );
         System.out.println( "This is karthik");
         System.out.println( "How are you ");
